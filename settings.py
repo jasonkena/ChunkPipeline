bce47aa5de4ba82358c125e0ead67d1d26371676
Jason Adhinarta <jasonice6@gmail.com>
2022-04-25 19:58:33 -0400

    Remove deprecated scripts

--- settings.py
+++ settings.py
@@ -6,9 +6,6 @@ ANISOTROPY = (30, 6, 6)
 # how many attempts to load chunk
 NUM_RETRY = 3
 
-
-REDIS_URL = "redis://:CR2Oe76Mrl8UTOBop7ulmklF80kXJbc1@redis-11424.c277.us-east-1-3.ec2.cloud.redislabs.com:11424"
-
 # baseline related hyperparameters
 CONNECTIVITY = 26
 MAX_ERODE = 100
