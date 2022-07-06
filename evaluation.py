6bdcdb87ef7322605eba4a16f2de34b03070bce3
Jason Adhinarta <jasonice6@gmail.com>
2022-07-06 13:08:23 -0400

    Working tests

--- evaluation.py
+++ evaluation.py
@@ -6,55 +6,46 @@ from tqdm import tqdm
 from scipy.optimize import linear_sum_assignment
 from skimage.measure import regionprops
 from collections import namedtuple
-from csbdeep.utils import _raise
+
+import dask
+import dask.array as da
+import chunk
 
 matching_criteria = dict()
 
 
-def label_are_sequential(y):
-    """returns true if y has only sequential labels from 1..."""
-    labels = np.unique(y)
-    return (set(labels) - {0}) == set(range(1, 1 + labels.max()))
+@dask.delayed
+def _chunk_relabel(unique):
+    remapping = np.arange(len(unique))
+    inverse_map = unique
+
+    if len(unique) and unique[0] != 0:
+        remapping += 1
+        inverse_map = np.concatenate([[0], inverse_map])
+
+    return remapping, inverse_map
 
 
-def is_array_of_integers(y):
-    return isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.integer)
+def chunk_relabel_sequential(vol):
+    # remap integers to lowest possible integers while preserving original order
+    # whilst treating 0 as background
+    unique, inverse = da.unique(vol, return_inverse=True)
+    temp = _chunk_relabel(unique)
+    remapping, inverse_map = [
+        da.from_delayed(temp[i], shape=[np.nan], dtype=vol.dtype) for i in range(2)
+    ]
+
+    remapped = chunk.chunk_remap(inverse, remapping)
+    return remapped, inverse_map
 
 
-def _check_label_array(y, name=None, check_sequential=False):
-    err = ValueError(
-        "{label} must be an array of {integers}.".format(
-            label="labels" if name is None else name,
-            integers=("sequential " if check_sequential else "")
-            + "non-negative integers",
-        )
-    )
-    is_array_of_integers(y) or _raise(err)
-    if len(y) == 0:
-        return True
-    if check_sequential:
-        label_are_sequential(y) or _raise(err)
-    else:
-        y.min() >= 0 or _raise(err)
-    return True
-
-
-def label_overlap(x, y, check=True):
-    if check:
-        _check_label_array(x, "x", True)
-        _check_label_array(y, "y", True)
-        x.shape == y.shape or _raise(ValueError("x and y must have the same shape"))
-    return _label_overlap(x, y)
-
-
-@jit(nopython=True)
-def _label_overlap(x, y):
+def label_overlap(x, y, x_max, y_max, block_info):
     x = x.ravel()
     y = y.ravel()
-    overlap = np.zeros((1 + x.max(), 1 + y.max()), dtype=np.uint)
+    overlap = np.zeros((1 + x_max, 1 + y_max), dtype=np.uint)
     for i in range(len(x)):
         overlap[x[i], y[i]] += 1
-    return overlap
+    return [overlap]
 
 
 def _safe_divide(x, y, eps=1e-10):
@@ -68,7 +59,6 @@ def _safe_divide(x, y, eps=1e-10):
 
 
 def intersection_over_union(overlap):
-    _check_label_array(overlap, "overlap")
     if np.sum(overlap) == 0:
         return overlap
     n_pixels_pred = np.sum(overlap, axis=0, keepdims=True)
@@ -80,7 +70,6 @@ matching_criteria["iou"] = intersection_over_union
 
 
 def intersection_over_true(overlap):
-    _check_label_array(overlap, "overlap")
     if np.sum(overlap) == 0:
         return overlap
     n_pixels_true = np.sum(overlap, axis=1, keepdims=True)
@@ -91,7 +80,6 @@ matching_criteria["iot"] = intersection_over_true
 
 
 def intersection_over_pred(overlap):
-    _check_label_array(overlap, "overlap")
     if np.sum(overlap) == 0:
         return overlap
     n_pixels_pred = np.sum(overlap, axis=0, keepdims=True)
@@ -120,7 +108,39 @@ def f1(tp, fp, fn):
     return (2 * tp) / (2 * tp + fp + fn) if tp > 0 else 0
 
 
-def matching(y_true, y_pred, thresh=0.5, criterion="iou", report_matches=False):
+@dask.delayed
+def _aggregate_overlap(overlap):
+    stacked = np.stack(overlap.flatten().tolist(), axis=0)
+    return np.sum(stacked, axis=0)
+
+
+def get_scores(y_true, y_pred, criterion="iou"):
+    assert y_true.shape == y_pred.shape
+    assert criterion in matching_criteria
+
+    y_true, map_rev_true = chunk_relabel_sequential(y_true)
+    y_pred, map_rev_pred = chunk_relabel_sequential(y_pred)
+
+    overlap = chunk.chunk(
+        label_overlap,
+        [y_true, y_pred],
+        output_dataset_dtypes=[object],
+        x_max=da.max(y_true),
+        y_max=da.max(y_pred),
+    )
+
+    overlap = da.from_delayed(
+        _aggregate_overlap(overlap), shape=[np.nan, np.nan], dtype=float
+    )
+    scores = dask.delayed(matching_criteria[criterion])(overlap)
+    scores = da.from_delayed(scores, shape=overlap.shape, dtype=overlap.dtype)
+
+    return scores, map_rev_true, map_rev_pred, criterion
+
+
+def matching(
+    scores, map_rev_true, map_rev_pred, criterion, thresh=0.5, report_matches=False
+):
     """Calculate detection/instance segmentation metrics between ground truth and predicted label images.
 
     Currently, the following metrics are implemented:
@@ -164,35 +184,19 @@ def matching(y_true, y_pred, thresh=0.5, criterion="iou", report_matches=False):
     Matching(criterion='iou', thresh=0.5, fp=1, tp=0, fn=1, precision=0, recall=0, accuracy=0, f1=0, n_true=1, n_pred=1, mean_true_score=0.0, mean_matched_score=0.0, panoptic_quality=0.0)
 
     """
-    _check_label_array(y_true, "y_true")
-    _check_label_array(y_pred, "y_pred")
-    y_true.shape == y_pred.shape or _raise(
-        ValueError(
-            "y_true ({y_true.shape}) and y_pred ({y_pred.shape}) have different shapes".format(
-                y_true=y_true, y_pred=y_pred
-            )
-        )
-    )
-    criterion in matching_criteria or _raise(
-        ValueError("Matching criterion '%s' not supported." % criterion)
-    )
     if thresh is None:
         thresh = 0
-    thresh = float(thresh) if np.isscalar(thresh) else map(float, thresh)
+    thresh = float(thresh) if np.isscalar(thresh) else list(map(float, thresh))
 
-    y_true, _, map_rev_true = relabel_sequential(y_true)
-    y_pred, _, map_rev_pred = relabel_sequential(y_pred)
+    @dask.delayed
+    def _single(scores, map_rev_true, map_rev_pred, thr):
+        assert 0 <= np.min(scores) <= np.max(scores) <= 1
 
-    overlap = label_overlap(y_true, y_pred, check=False)
-    scores = matching_criteria[criterion](overlap)
-    assert 0 <= np.min(scores) <= np.max(scores) <= 1
+        # ignoring background
+        scores = scores[1:, 1:]
+        n_true, n_pred = scores.shape
+        n_matched = min(n_true, n_pred)
 
-    # ignoring background
-    scores = scores[1:, 1:]
-    n_true, n_pred = scores.shape
-    n_matched = min(n_true, n_pred)
-
-    def _single(thr):
         # not_trivial = n_matched > 0 and np.any(scores >= thr)
         not_trivial = n_matched > 0
         if not_trivial:
@@ -255,309 +259,9 @@ def matching(y_true, y_pred, thresh=0.5, criterion="iou", report_matches=False):
                 )
         return namedtuple("Matching", stats_dict.keys())(*stats_dict.values())
 
-    return _single(thresh) if np.isscalar(thresh) else tuple(map(_single, thresh))
-
-
-def matching_dataset(
-    y_true,
-    y_pred,
-    thresh=0.5,
-    criterion="iou",
-    by_image=False,
-    show_progress=True,
-    parallel=False,
-):
-    """matching metrics for list of images, see `stardist.matching.matching`"""
-    len(y_true) == len(y_pred) or _raise(
-        ValueError("y_true and y_pred must have the same length.")
-    )
-    return matching_dataset_lazy(
-        tuple(zip(y_true, y_pred)),
-        thresh=thresh,
-        criterion=criterion,
-        by_image=by_image,
-        show_progress=show_progress,
-        parallel=parallel,
-    )
-
-
-def matching_dataset_lazy(
-    y_gen,
-    thresh=0.5,
-    criterion="iou",
-    by_image=False,
-    show_progress=True,
-    parallel=False,
-):
-
-    expected_keys = set(
-        (
-            "fp",
-            "tp",
-            "fn",
-            "precision",
-            "recall",
-            "accuracy",
-            "f1",
-            "criterion",
-            "thresh",
-            "n_true",
-            "n_pred",
-            "mean_true_score",
-            "mean_matched_score",
-            "panoptic_quality",
-        )
-    )
-
-    single_thresh = False
     if np.isscalar(thresh):
-        single_thresh = True
-        thresh = (thresh,)
+        thresh = [thresh]
 
-    tqdm_kwargs = {}
-    tqdm_kwargs["disable"] = not bool(show_progress)
-    if int(show_progress) > 1:
-        tqdm_kwargs["total"] = int(show_progress)
+    result = [_single(scores, map_rev_true, map_rev_pred, x) for x in thresh]
 
-    # compute matching stats for every pair of label images
-    if parallel:
-        from concurrent.futures import ThreadPoolExecutor
-
-        fn = lambda pair: matching(
-            *pair, thresh=thresh, criterion=criterion, report_matches=False
-        )
-        with ThreadPoolExecutor() as pool:
-            stats_all = tuple(pool.map(fn, tqdm(y_gen, **tqdm_kwargs)))
-    else:
-        stats_all = tuple(
-            matching(y_t, y_p, thresh=thresh, criterion=criterion, report_matches=False)
-            for y_t, y_p in tqdm(y_gen, **tqdm_kwargs)
-        )
-
-    # accumulate results over all images for each threshold separately
-    n_images, n_threshs = len(stats_all), len(thresh)
-    accumulate = [{} for _ in range(n_threshs)]
-    for stats in stats_all:
-        for i, s in enumerate(stats):
-            acc = accumulate[i]
-            for k, v in s._asdict().items():
-                if k == "mean_true_score" and not bool(by_image):
-                    # convert mean_true_score to "sum_matched_score"
-                    acc[k] = acc.setdefault(k, 0) + v * s.n_true
-                else:
-                    try:
-                        acc[k] = acc.setdefault(k, 0) + v
-                    except TypeError:
-                        pass
-
-    # normalize/compute 'precision', 'recall', 'accuracy', 'f1'
-    for thr, acc in zip(thresh, accumulate):
-        set(acc.keys()) == expected_keys or _raise(ValueError("unexpected keys"))
-        acc["criterion"] = criterion
-        acc["thresh"] = thr
-        acc["by_image"] = bool(by_image)
-        if bool(by_image):
-            for k in (
-                "precision",
-                "recall",
-                "accuracy",
-                "f1",
-                "mean_true_score",
-                "mean_matched_score",
-                "panoptic_quality",
-            ):
-                acc[k] /= n_images
-        else:
-            tp, fp, fn, n_true = acc["tp"], acc["fp"], acc["fn"], acc["n_true"]
-            sum_matched_score = acc["mean_true_score"]
-
-            mean_matched_score = _safe_divide(sum_matched_score, tp)
-            mean_true_score = _safe_divide(sum_matched_score, n_true)
-            panoptic_quality = _safe_divide(sum_matched_score, tp + fp / 2 + fn / 2)
-
-            acc.update(
-                precision=precision(tp, fp, fn),
-                recall=recall(tp, fp, fn),
-                accuracy=accuracy(tp, fp, fn),
-                f1=f1(tp, fp, fn),
-                mean_true_score=mean_true_score,
-                mean_matched_score=mean_matched_score,
-                panoptic_quality=panoptic_quality,
-            )
-
-    accumulate = tuple(
-        namedtuple("DatasetMatching", acc.keys())(*acc.values()) for acc in accumulate
-    )
-    return accumulate[0] if single_thresh else accumulate
-
-
-# copied from scikit-image master for now (remove when part of a release)
-def relabel_sequential(label_field, offset=1):
-    """Relabel arbitrary labels to {`offset`, ... `offset` + number_of_labels}.
-
-    This function also returns the forward map (mapping the original labels to
-    the reduced labels) and the inverse map (mapping the reduced labels back
-    to the original ones).
-
-    Parameters
-    ----------
-    label_field : numpy array of int, arbitrary shape
-        An array of labels, which must be non-negative integers.
-    offset : int, optional
-        The return labels will start at `offset`, which should be
-        strictly positive.
-
-    Returns
-    -------
-    relabeled : numpy array of int, same shape as `label_field`
-        The input label field with labels mapped to
-        {offset, ..., number_of_labels + offset - 1}.
-        The data type will be the same as `label_field`, except when
-        offset + number_of_labels causes overflow of the current data type.
-    forward_map : numpy array of int, shape ``(label_field.max() + 1,)``
-        The map from the original label space to the returned label
-        space. Can be used to re-apply the same mapping. See examples
-        for usage. The data type will be the same as `relabeled`.
-    inverse_map : 1D numpy array of int, of length offset + number of labels
-        The map from the new label space to the original space. This
-        can be used to reconstruct the original label field from the
-        relabeled one. The data type will be the same as `relabeled`.
-
-    Notes
-    -----
-    The label 0 is assumed to denote the background and is never remapped.
-
-    The forward map can be extremely big for some inputs, since its
-    length is given by the maximum of the label field. However, in most
-    situations, ``label_field.max()`` is much smaller than
-    ``label_field.size``, and in these cases the forward map is
-    guaranteed to be smaller than either the input or output images.
-
-    Examples
-    --------
-    >>> from skimage.segmentation import relabel_sequential
-    >>> label_field = np.array([1, 1, 5, 5, 8, 99, 42])
-    >>> relab, fw, inv = relabel_sequential(label_field)
-    >>> relab
-    array([1, 1, 2, 2, 3, 5, 4])
-    >>> fw
-    array([0, 1, 0, 0, 0, 2, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5])
-    >>> inv
-    array([ 0,  1,  5,  8, 42, 99])
-    >>> (fw[label_field] == relab).all()
-    True
-    >>> (inv[relab] == label_field).all()
-    True
-    >>> relab, fw, inv = relabel_sequential(label_field, offset=5)
-    >>> relab
-    array([5, 5, 6, 6, 7, 9, 8])
-    """
-    offset = int(offset)
-    if offset <= 0:
-        raise ValueError("Offset must be strictly positive.")
-    if np.min(label_field) < 0:
-        raise ValueError("Cannot relabel array that contains negative values.")
-    max_label = int(label_field.max())  # Ensure max_label is an integer
-    if not np.issubdtype(label_field.dtype, np.integer):
-        new_type = np.min_scalar_type(max_label)
-        label_field = label_field.astype(new_type)
-    labels = np.unique(label_field)
-    labels0 = labels[labels != 0]
-    new_max_label = offset - 1 + len(labels0)
-    new_labels0 = np.arange(offset, new_max_label + 1)
-    output_type = label_field.dtype
-    required_type = np.min_scalar_type(new_max_label)
-    if np.dtype(required_type).itemsize > np.dtype(label_field.dtype).itemsize:
-        output_type = required_type
-    forward_map = np.zeros(max_label + 1, dtype=output_type)
-    forward_map[labels0] = new_labels0
-    inverse_map = np.zeros(new_max_label + 1, dtype=output_type)
-    inverse_map[offset:] = labels0
-    relabeled = forward_map[label_field]
-    return relabeled, forward_map, inverse_map
-
-
-def group_matching_labels(ys, thresh=1e-10, criterion="iou"):
-    """
-    Group matching objects (i.e. assign the same label id) in a
-    list of label images (e.g. consecutive frames of a time-lapse).
-
-    Uses function `matching` (with provided `criterion` and `thresh`) to
-    iteratively/greedily match and group objects/labels in consecutive images of `ys`.
-    To that end, matching objects are grouped together by assigning the same label id,
-    whereas unmatched objects are assigned a new label id.
-    At the end of this process, each label group will have been assigned a unique id.
-
-    Note that the label images `ys` will not be modified. Instead, they will initially
-    be duplicated and converted to data type `np.int32` before objects are grouped and the result
-    is returned. (Note that `np.int32` limits the number of label groups to at most 2147483647.)
-
-    Example
-    -------
-    import numpy as np
-    from stardist.data import test_image_nuclei_2d
-    from stardist.matching import group_matching_labels
-
-    _y = test_image_nuclei_2d(return_mask=True)[1]
-    labels = np.stack([_y, 2*np.roll(_y,10)], axis=0)
-
-    labels_new = group_matching_labels(labels)
-
-    Parameters
-    ----------
-    ys : np.ndarray or list/tuple of np.ndarray
-        list/array of integer labels (2D or 3D)
-
-    """
-    # check 'ys' without making a copy
-    len(ys) > 1 or _raise(ValueError("'ys' must have 2 or more entries"))
-    if isinstance(ys, np.ndarray):
-        _check_label_array(ys, "ys")
-        ys.ndim > 1 or _raise(ValueError("'ys' must be at least 2-dimensional"))
-        ys_grouped = np.empty_like(ys, dtype=np.int32)
-    else:
-        all(_check_label_array(y, "ys") for y in ys) or _raise(
-            ValueError("'ys' must be a list of label images")
-        )
-        all(y.shape == ys[0].shape for y in ys) or _raise(
-            ValueError("all label images must have the same shape")
-        )
-        ys_grouped = np.empty((len(ys),) + ys[0].shape, dtype=np.int32)
-
-    def _match_single(y_prev, y, next_id):
-        y = y.astype(np.int32, copy=False)
-        res = matching(
-            y_prev, y, report_matches=True, thresh=thresh, criterion=criterion
-        )
-        # relabel dict (for matching labels) that maps label ids from y -> y_prev
-        relabel = dict(reversed(res.matched_pairs[i]) for i in res.matched_tps)
-        y_grouped = np.zeros_like(y)
-        for r in regionprops(y):
-            m = y[r.slice] == r.label
-            if r.label in relabel:
-                y_grouped[r.slice][m] = relabel[r.label]
-            else:
-                y_grouped[r.slice][m] = next_id
-                next_id += 1
-        return y_grouped, next_id
-
-    ys_grouped[0] = ys[0]
-    next_id = ys_grouped[0].max() + 1
-    for i in range(len(ys) - 1):
-        ys_grouped[i + 1], next_id = _match_single(ys_grouped[i], ys[i + 1], next_id)
-    return ys_grouped
-
-
-def _shuffle_labels(y):
-    _check_label_array(y, "y")
-    y2 = np.zeros_like(y)
-    ids = tuple(set(np.unique(y)) - {0})
-    relabel = dict(zip(ids, np.random.permutation(ids)))
-    for r in regionprops(y):
-        m = y[r.slice] == r.label
-        y2[r.slice][m] = relabel[r.label]
-    return y2
+    return result if len(result) > 1 else result[0]
