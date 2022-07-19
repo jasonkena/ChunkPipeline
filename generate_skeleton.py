3939fd0a45852bf6690bc257e17d397ba3f9c66f
Jason Adhinarta <jasonice6@gmail.com>
2022-07-19 00:44:21 -0400

    Bugfix for skeletonization, take into account anisotropy in h5 result

--- generate_skeleton.py
+++ generate_skeleton.py
@@ -9,11 +9,16 @@ from settings import *
 
 
 def main(base_path, id):
-    input = h5py.File(os.path.join(base_path, f"{str(id)}.h5")).get("main")
+    input = h5py.File(os.path.join(base_path, f"{str(id)}.h5"))
+    row = input.get("row")[:]
     # read into memory, may need to refactor if out of memory
-    input = input[:]
+    input = input.get("main")[:]
 
     seg_skeleton = kimimaro.skeletonize(input, **KIMI_PARAMS)[1]
+    # undo anisotropy
+    seg_skeleton.vertices /= np.array(ANISOTROPY)
+    # offset by bounding box
+    seg_skeleton.vertices += np.array([row[1], row[3], row[5]]).reshape(1, 3)
     # choose random point to find furthest point, using it as seed
     seed = skel.find_furthest_pt(seg_skeleton, 0, single=False)[0]
     longest_path = skel.find_furthest_pt(seg_skeleton, seed, single=False)[1][0]
