754bdcb512aa1fd5211609548b85e0f298b16cb5
Jason Adhinarta <jasonice6@gmail.com>
2024-08-06 14:51:51 -0400

    skeletonize trunks and spines using different params

--- scripts/igneous/skeletonize.py
+++ scripts/igneous/skeletonize.py
@@ -1,15 +1,31 @@
+import numpy as np
 from cloudvolume import Vec
 import igneous.task_creation as tc
 from taskqueue import LocalTaskQueue
 from utils import get_conf
+from visualize import read_mappings
 
 
 def main(conf):
     tq = LocalTaskQueue(parallel=conf.n_jobs_skeletonize)
 
+    mapping = np.load(conf.data.mapping)
+    seg_to_trunk, trunk_to_segs = read_mappings(mapping)
+
+    seg_ids = sorted(seg_to_trunk.keys())
+    trunk_ids = sorted(trunk_to_segs.keys())
+    spine_ids = sorted(list(set(seg_ids) - set(trunk_ids)))
+
     layer = f"file://{conf.data.output_layer}"
-    skeletonize_tasks = tc.create_skeletonizing_tasks(layer, **conf.skeletonize)
-    tq.insert(skeletonize_tasks)
+
+    skeletonize_trunk_tasks = tc.create_skeletonizing_tasks(
+        layer, **conf.skeletonize_trunk, object_ids=trunk_ids
+    )
+    tq.insert(skeletonize_trunk_tasks)
+    skeletonize_spines_tasks = tc.create_skeletonizing_tasks(
+        layer, **conf.skeletonize_spines, object_ids=spine_ids
+    )
+    tq.insert(skeletonize_spines_tasks)
     tq.execute()
 
     merge_tasks = tc.create_unsharded_skeleton_merge_tasks(
